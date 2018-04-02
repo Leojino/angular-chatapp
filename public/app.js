e9ed9e46757830322b3970fb387f74143611a9ef
Jino John <leojino@gmail.com>
2018-04-02 20:59:47 +0530

    add bottom scroller

--- public/app.js
+++ public/app.js
@@ -37,4 +37,23 @@ chatApp.controller('ChatRoomController', function($scope){
             msg_type: user == "user" ? "text_msg" : "text_msg_2"
         });
     } 
-})
+});
+
+chatApp.directive('scrollToBottom', function($timeout, $window) {
+    return {
+        scope: {
+            scrollToBottom: "="
+        },
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            scope.$watchCollection('scrollToBottom', function(newVal) {
+                if (newVal) {
+                    $timeout(function() {
+                        element[0].scrollTop =  element[0].scrollHeight;
+                    }, 0);
+                }
+
+            });
+        }
+    };
+});
